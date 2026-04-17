8674f5ae791a1f522b0dd514039544df4740b190
proyectohidroponico03-svg <proyectohidroponico03@gmail.com>
2026-04-17 04:13:27 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// firebase-config.js
+
+// Importa las funciones necesarias de Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA8ehAHx15-Oo_FBEt_J_NMG5rDvYicAY8",
+  authDomain: "proyecto-hidroponico-9ea9d.firebaseapp.com",
+  databaseURL: "https://proyecto-hidroponico-9ea9d-default-rtdb.firebaseio.com/",
+  projectId: "proyecto-hidroponico-9ea9d",
+  storageBucket: "proyecto-hidroponico-9ea9d.appspot.com",
+  messagingSenderId: "1080749567541",
+  appId: "1:1080749567541:web:e953df10935ccc67914b7c"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Obtener la referencia a la base de datos
+const db = getDatabase(app);
+
+export { db };
